b2530093b419acc9cf3151813e0c4b1867aeabad
Diego Pino García <dpino@igalia.com>
2012-09-04 16:31:56 +0000

    Script to create table of 'FoodAndDrinks' for PostgreSQL

---
+++ portlets/calendar-portlet/food_and_drinks.psql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE address (
+    id bigint SERIAL,
+    name text,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO FoodAndDrinks(name) VALUES('Coke');
+INSERT INTO FoodAndDrinks(name) VALUES('Snacks');
+INSERT INTO FoodAndDrinks(name) VALUES('Chips');
