67f70f94688247fe4171b395147bc0c8fbad2f37
Diego Pino García <dpino@igalia.com>
2012-09-05 10:11:12 +0000

    Fix script for creating 'Food and Drinks' table in PostgreSQL

--- portlets/calendar-portlet/food_and_drinks.psql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE address (
-    id bigint SERIAL,
-    name text,
-    PRIMARY KEY (id)
-);
-
-INSERT INTO FoodAndDrinks(name) VALUES('Coke');
-INSERT INTO FoodAndDrinks(name) VALUES('Snacks');
-INSERT INTO FoodAndDrinks(name) VALUES('Chips');
---
+++ portlets/calendar-portlet/sql/food_and_drinks.psql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE foodanddrinks (
+    id SERIAL,
+    name text,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO foodanddrinks(name) VALUES('Coke');
+INSERT INTO foodanddrinks(name) VALUES('Snacks');
+INSERT INTO foodanddrinks(name) VALUES('Chips');
